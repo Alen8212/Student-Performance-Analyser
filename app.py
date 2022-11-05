0d8bd98579f83b3d43222683179587c71a9fd1aa
Alen George <93701441+Alen8212@users.noreply.github.com>
2022-11-05 14:14:28 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,44 @@
+import json
+from flask import Flask, render_template ,jsonify
+import os
+from flask import request
+
+import numpy as np
+import pickle
+
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(__name__ , template_folder="templates")
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route('/', methods=['POST','GET'])
+def home():
+    return render_template("index.html")
+
+
+
+
+@app.route("/predict", methods = ["POST"])
+def predict():
+    float_features = []
+    for x in request.form.values():
+        if not x.isnumeric():
+            response = {"status" : 500,"status_msg": "Some fields are empty !"}
+            return jsonify(response)
+
+        float_features.append(int(x))
+
+
+    final = [np.array(float_features)]
+    prediction = model.predict(final)
+
+    response = {"status" : 200,"status_msg": "Student may Fail !!!"}
+
+
+    if prediction == "Pass":
+        response = {"status" : 200,"status_msg": "Student will Pass !!!"}
+      
+    return jsonify(response)
+if __name__ == '__main__':
+    app.run(debug=True)
